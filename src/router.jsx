27862f751020d72f2d489c16888d32813916df30
Tiffany Chen <tiffany.chen@104.com.tw>
2022-12-22 16:58:24 +0800

    setup

---
+++ src/router.jsx
@@ -0,0 +1,33 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: ""
+  },
+  {
+    path: "/login",
+    element: ""
+  },
+  {
+    path: "/forgot-password",
+    element: ""
+  },
+  {
+    path: "/signup",
+    element: ""
+  },
+  {
+    path: "/change-password",
+    element: ""
+  },
+  {
+    path: "/profile",
+    element: ""
+  },
+])
